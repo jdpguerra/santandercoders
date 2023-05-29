0e8b8feef695c3e62549332dc2b201a89e3051c5
Jose Guerra <josedavidportillo@gmail.com>
2023-05-29 11:32:32 -0300

    finish course ADA
    finish course

---
+++ meu-react/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+function App() {
+
+
+  return (
+    <div>
+    <h1>Ola mundo react1</h1>
+    <MeuComponente/>
+    <MeuComponente/>
+    <MeuComponente/>
+    <MeuComponente/>
+
+
+
+    </div>
+
+  )
+}
+
+function MeuComponente() {
+  return (
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, ipsum, laboriosam quo ipsam necessitatibus quidem beatae labore neque error accusamus totam soluta perspiciatis nulla voluptate recusandae at nobis placeat, explicabo possimus a iure. Dolorem rerum ex architecto officiis voluptas atque laboriosam velit temporibus minus! Natus fugiat ut repellendus assumenda sunt!</p>
+  )
+}
+export default App
